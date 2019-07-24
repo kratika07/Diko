98274698ff117d25a19b144dccb5d8d4feb70e69
Covetus <kratika.v.covetus@gmail.com>
2019-07-24 15:21:16 +0530

    test

---
+++ app/src/main/java/com/example/diko/Activity/Testctivity.java
@@ -0,0 +1,15 @@
+package com.example.diko.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.diko.R;
+
+public class Testctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testctivity);
+    }
+}
